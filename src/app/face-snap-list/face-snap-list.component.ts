46a7d4c0d4c2e33148c1fb09a136f6a1b2c4e3b5
Souliman <souleymane.sada@gmail.com>
2023-01-10 14:51:30 +0000

    Strategie de unsubscribe: avec l'operateur take

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, take, tap } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
-  styleUrls: ['./face-snap-list.component.scss']
+  styleUrls: ['./face-snap-list.component.scss'],
 })
 export class FaceSnapListComponent implements OnInit {
-  
-  faceSnaps!:FaceSnap[];
+  faceSnaps!: FaceSnap[];
 
-  constructor(private faceSnapsServie: FaceSnapsService) { }
+  constructor(private faceSnapsServie: FaceSnapsService) {}
 
   ngOnInit(): void {
-    
     this.faceSnaps = this.faceSnapsServie.getAllFaceSnaps();
-  }
 
+    interval(1000)
+      .pipe(
+        take(2),
+        tap((value) => console.log(value))
+      )
+      .subscribe();
+  }
 }
