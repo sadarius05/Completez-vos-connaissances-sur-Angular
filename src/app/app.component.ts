7f7b574f731ef314238a2cfe0891ad121796ce7a
Souliman <souleymane.sada@gmail.com>
2023-01-10 00:41:07 +0000

    Decouverte des Observables avec intervalle() et setTimeOut()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  implements OnInit{
-  
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const interval$ = interval(1000);
 
-  ngOnInit(): void {}
+    interval$.subscribe((value) => console.log(value));
+    setTimeout(() => interval$.subscribe((value) => console.log(value)), 3000);
+  }
 }
